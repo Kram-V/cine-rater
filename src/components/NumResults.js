d48ddeaecdaa4d407b2b7c58f69f441988a3d7fe
Kram-V <markvivar32@gmail.com>
2024-10-29 18:40:23 +0800

    removed console.log

--- src/components/NumResults.js
+++ src/components/NumResults.js
@@ -1,8 +1,6 @@
 import React from "react";
 
 const NumResults = ({ movies }) => {
-  console.log("MOVIES NUM RESULTS: ", movies);
-
   return (
     <p className="num-results">
       Found <strong>{movies?.length}</strong> results
